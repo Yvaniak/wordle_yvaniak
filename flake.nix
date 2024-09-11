b2bf9d739d057f868fc3e948aefc80e4773a95fe
Yvaniak <yvaniak11@gmail.com>
2024-09-11 05:34:40 +0200

    nix fmt

--- flake.nix
+++ flake.nix
@@ -1,75 +1,75 @@
 {
-description = "wordle-yvaniak";
+  description = "wordle-yvaniak";
 
-inputs = {
-  flake-utils = {
-    url = "github:numtide/flake-utils";
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
-};
 
-outputs = { self, nixpkgs, ... }@inputs:
-  inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      formatter.pkgs = pkgs.nixpkgs-fmt;
-
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [ self.packages.${pkgs.system}.default ];
-        packages = [
-          #voir la taille des grosses deps
-          pkgs.cargo-bloat
-          #gerer les deps depuis le cli
-          pkgs.cargo-edit
-          #trouver les outdated
-          pkgs.cargo-outdated
-          #trouver les deps non utilisés (à besoin de nightly)
-          pkgs.cargo-udeps
-          #auto compile
-          pkgs.cargo-watch
-          #lsp
-          pkgs.rust-analyzer
-          #lint
-          pkgs.clippy
-          #fmt rust
-          pkgs.rustfmt
-          #fmt nix
-          pkgs.nixpkgs-fmt
-        ];
-
-        env = {
-          RUST_BACKTRACE = "1";
+  outputs = { self, nixpkgs, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
+      in
+      {
+        formatter.pkgs = pkgs.nixpkgs-fmt;
 
-        shellHook = ''
-          echo "shell pour wordle"
-        '';
-      };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${pkgs.system}.default ];
+          packages = [
+            #voir la taille des grosses deps
+            pkgs.cargo-bloat
+            #gerer les deps depuis le cli
+            pkgs.cargo-edit
+            #trouver les outdated
+            pkgs.cargo-outdated
+            #trouver les deps non utilisés (à besoin de nightly)
+            pkgs.cargo-udeps
+            #auto compile
+            pkgs.cargo-watch
+            #lsp
+            pkgs.rust-analyzer
+            #lint
+            pkgs.clippy
+            #fmt rust
+            pkgs.rustfmt
+            #fmt nix
+            pkgs.nixpkgs-fmt
+          ];
 
-      packages = {
-        default = pkgs.rustPlatform.buildRustPackage {
-          pname = "wordle-yvaniak";
-          version = "0.1.0";
-
-          nativeBuildInputs = [ pkgs.rustc pkgs.cargo ];
-
-          src = ./.;
-
-          cargoHash = "sha256-w0fwlAcHwGGyoL3UEPPux6uglOLabj5orFXP3EAV2zI=";
-
-          meta = with pkgs.stdenv.lib; {
-            description = "A simple wordle tui and gui";
-            homepage = "https://github.com/Yvaniak/wordle-yvaniak";
-            licence = licences.MIT;
-            mainteners = [ mainteners.yvaniak ];
+          env = {
+            RUST_BACKTRACE = "1";
           };
+
+          shellHook = ''
+            echo "shell pour wordle"
+          '';
         };
 
-        wordle-yvaniak= self.packages.${pkgs.system}.default;
-      };
-    }
-  );
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "wordle-yvaniak";
+            version = "0.1.0";
+
+            nativeBuildInputs = [ pkgs.rustc pkgs.cargo ];
+
+            src = ./.;
+
+            cargoHash = "sha256-w0fwlAcHwGGyoL3UEPPux6uglOLabj5orFXP3EAV2zI=";
+
+            meta = with pkgs.stdenv.lib; {
+              description = "A simple wordle tui and gui";
+              homepage = "https://github.com/Yvaniak/wordle-yvaniak";
+              licence = licences.MIT;
+              mainteners = [ mainteners.yvaniak ];
+            };
+          };
+
+          wordle-yvaniak = self.packages.${pkgs.system}.default;
+        };
+      }
+    );
 }
