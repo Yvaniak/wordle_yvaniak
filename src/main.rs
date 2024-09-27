86880449e8948b65d29923538decdf413ccac929
Yvaniak <yvaniak11@gmail.com>
2024-09-27 17:49:30 +0200

    chg: avancement misplace

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,8 @@ fn main() {
         let choix: &str = choix.trim();
 
         match choix {
-            "s" | "start" => boucler = partie(pick_the_word()),
+            // "s" | "start" => boucler = partie(pick_the_word()),
+            "s" | "start" => boucler = partie("brass".to_string()),
             "q" | "quit" | "exit" | "e" => boucler = false,
             _ => println!("didn't understood that, can you repeat ?"),
         }
@@ -133,22 +134,29 @@ fn partie(mot: String) -> bool {
 //SANDS : 22001 ; TURNS : 00201 ; SUPER : 20002 ; CARBS : 02221 ; BARBS : 12201 ; CANAL : 02020
 //et téééphone pour téléphone
 fn is_misplaced(mot: String, guess: String) {
-    let mut pos: u16 = 1;
+    let mut pos: usize = 1;
     let mut mot_copy: String = mot.clone();
     for c_guess in guess.clone().chars() {
         let c_mot: char = mot_copy.remove(0);
 
         if c_guess != c_mot {
             let mut misplaced = false;
+            let mut vec_mot: Vec<usize> = Vec::new();
+            let mut vec_guess: Vec<usize> = Vec::new();
             let mut mot_copy_counts = mot.clone();
             let mut guess_copy_counts = guess.clone();
             let mut lettre_guess_exists: Option<usize> = Some(1);
+            //TODO: maybe use des matchs pour gerer les options
             while Option::is_some(&lettre_guess_exists) {
-                lettre_guess_exists = guess_copy_counts.find(c_guess);
-                let lettre_mot_exists = mot_copy_counts.find(c_guess);
+                lettre_guess_exists = guess_copy_counts.rfind(c_guess);
+                let lettre_mot_exists = mot_copy_counts.rfind(c_guess);
                 if Option::is_some(&lettre_guess_exists) {
+                    vec_guess.push(lettre_guess_exists.unwrap());
+                    println!("avant {}", guess_copy_counts);
                     guess_copy_counts.remove(lettre_guess_exists.unwrap());
+                    println!("après {}", guess_copy_counts);
                     if Option::is_some(&lettre_mot_exists) {
+                        vec_mot.push(lettre_mot_exists.unwrap());
                         mot_copy_counts.remove(lettre_mot_exists.unwrap());
                         if lettre_mot_exists.unwrap() != lettre_guess_exists.unwrap() {
                             misplaced = true;
@@ -158,10 +166,46 @@ fn is_misplaced(mot: String, guess: String) {
                 } else {
                 }
             }
+            let mut pas_exists_misplace = true;
+            //pas_exists_misplace = !vec_mot.contains(&(pos-1));
+            // println!("vec guess : ");
+            for v in vec_guess.clone() {
+                // println!("{v}");
+            }
+            // println!("vec mot : ");
+            let mut vec_guess_copy = vec_guess.clone();
+            for v_mot_i in 0..vec_mot.len() {
+                println!("{}", vec_mot[v_mot_i]);
+                //if misplaced && v!=(pos-1){
+                //    //FIX : pour tous
+                //    // pas_exists_misplace = false;
+                //}
+                let letter_found = false;
+                if vec_guess_copy.contains(&vec_mot[v_mot_i]) {
+                    for v_guess_i in 0..vec_guess_copy.len() {
+                        if vec_guess[v_guess_i] == vec_mot[v_mot_i] && !letter_found {
+                            vec_guess_copy.remove(v_guess_i);
+                        }
+                    }
+                }
+            }
+            if vec_guess_copy.len() != 0 {
+                pas_exists_misplace = false;
+                for v in vec_guess_copy {
+                    // println!("vec_guess_copy {}", v);
+                }
+            }
+            // println!("misplaced {}", misplaced);
+            // println!("pas_exists_misplace {}", pas_exists_misplace);
+
             //FIX: si des lettres sont bien placées et qu'il y a une autre lettre en plus comme
             //"baass" pour "brass" ou "téééphone" pour "téléphone"
             //il faut que ce soit not good au lieu de misplaced""
-            if misplaced {
+            //rfind résout le problème de gauche à droite mais le droite à gauche
+            //apparaît
+            //mais barbs marche plus
+            //barss marche pas non plus
+            if misplaced && pas_exists_misplace {
                 println!("\nThe letter {} in position {} is misplaced", c_guess, pos);
             } else {
                 println!("\nThe letter {} in position {} is not good", c_guess, pos);
