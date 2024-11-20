66922182b21bf1fcf3c48538fa839c79a49d1b81
Yvaniak <yvaniak11@gmail.com>
2024-11-20 23:24:29 +0100

    add: nix flake check with a lot of checks from crane

--- flake.nix
+++ flake.nix
@@ -7,9 +7,16 @@
     };
 
     naersk.url = "github:nix-community/naersk";
+
+    crane.url = "github:ipetkov/crane";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, advisory-db, ... }@inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -18,14 +25,79 @@
 
         naersk' = pkgs.callPackage inputs.naersk { };
 
+        craneLib = inputs.crane.mkLib pkgs;
+        src = ./.;
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src;
+        };
+
+        crateName = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
+
+        # Run clippy (and deny all warnings) on the crate source,
+        # resuing the dependency artifacts (e.g. from build scripts or
+        # proc-macros) from above.
+        #
+        # Note that this is done as a separate derivation so it
+        # does not impact building just the crate by itself.
+        my-crate-clippy = craneLib.cargoClippy {
+          inherit cargoArtifacts src;
+          cargoClippyExtraArgs = "-- --deny warnings";
+          buildPhaseCargoCommand = "cargo clippy --profile release && cargo fix";
+        };
+
+        my-crate-cargo-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
+        };
+
+        my-crate-cargo-doc = craneLib.cargoDoc {
+          inherit cargoArtifacts src;
+        };
+
+        my-crate-cargo-doc-test = craneLib.cargoDocTest {
+          inherit cargoArtifacts src;
+        };
+
+        my-crate-cargo-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+
+        my-crate-cargo-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        my-crate-taplo-fmt = craneLib.taploFmt {
+          inherit src;
+        };
+
+        my-crate-cargo-llvm-cov = craneLib.cargoLlvmCov {
+          inherit cargoArtifacts src;
+        };
+
+        my-crate-cargo-nextest = craneLib.cargoNextest {
+          inherit cargoArtifacts src;
+        };
+
+        my-crane-cargo-update = craneLib.buildPackage {
+          inherit cargoArtifacts src;
+          cargoBuildCommand = "cargo update && cargo build --profile release";
+        };
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        my-crate = craneLib.buildPackage {
+          inherit cargoArtifacts src;
+        };
+
+        # Also run the crate tests under cargo-tarpaulin so that we can keep
+        # track of code coverage
+        my-crate-coverage = craneLib.cargoTarpaulin {
+          inherit cargoArtifacts src;
+        };
+
         mylib = {
-          fmt = pkgs.writeShellApplication {
-            name = "fmt";
-            text = ''
-              nixpkgs-fmt .
-              cargo fmt
-            '';
-          };
           lint = pkgs.writeShellApplication {
             name = "lint";
             text = ''
@@ -65,7 +137,6 @@
             pkgs.cargo-deny
 
             #scripts utilitaires
-            mylib.fmt
             mylib.lint
           ];
 
@@ -79,6 +150,7 @@
         };
 
         packages = {
+          crane-test = my-crate;
           default = naersk'.buildPackage {
             nativeBuildInputs = [ pkgs.rustc pkgs.cargo ];
 
@@ -94,6 +166,22 @@
           };
 
           wordle_yvaniak = self.packages.${pkgs.system}.default;
+        };
+        
+        checks = {
+          inherit
+            my-crate
+            my-crate-clippy
+            my-crate-coverage
+            my-crate-cargo-audit
+            my-crate-cargo-deny
+            my-crate-cargo-doc
+            my-crate-cargo-doc-test
+            my-crate-cargo-fmt
+            my-crate-cargo-llvm-cov
+            my-crate-cargo-nextest
+            my-crate-taplo-fmt
+            my-crane-cargo-update;
         };
       }
     );
