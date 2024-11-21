dd4007a87e370984550f79ad97bf367154e9b7b1
Yvaniak <yvaniak11@gmail.com>
2024-11-21 13:25:29 +0100

    Woops forgot

--- flake.nix
+++ flake.nix
@@ -170,7 +170,7 @@
             wordle_yvaniak-taplo-fmt;
         };
 
-        # packages.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix { checks = inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks; };
+        packages.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix { checks = inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks; };
       }
     );
 }
