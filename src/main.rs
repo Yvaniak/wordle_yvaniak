eec201a6e5bc9f2a32f44b8473a81fd464bd8504
Yvaniak <yvaniak11@gmail.com>
2024-11-21 13:43:14 +0100

    Del: tests main commented by a todo

--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,5 @@ fn main() {
         process::exit(1);
     }
 }
-#[cfg(test)]
-mod test {
 
-    //    #[test]
-    //    fn cmd_config_cli() {
-    //        let cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))
-    //            .unwrap()
-    //            .arg("cli")
-    //            .write_stdin("q")
-    //            .assert();
-    //        cmd.success()
-    //            .stdout("Welcome in the menu of this worlde game !\nexitting\n");
-    //    }
-
-    //    //TODO: tester avec tui
-
-    //    #[test]
-    //    fn cmd_config_gui() {
-    //        let cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))
-    //            .unwrap()
-    //            .arg("gui")
-    //            .assert();
-    //        cmd.failure().code(1);
-    //    }
-
-    //    #[test]
-    //    fn cmd_config_unknown() {
-    //        let cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))
-    //            .unwrap()
-    //            .arg("unknown")
-    //            .assert();
-    //        // assert_eq!("", cmd.failure().code(2).get_output());
-    //        cmd.failure().code(2);
-    //    }
-}
+//TODO: Test main
